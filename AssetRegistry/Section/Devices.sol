05a9f04e50973e0407c6482a4c60e69542dbf67e
SamTylor <92628486+SamTylor@users.noreply.github.com>
2021-11-19 12:41:36 +0200

    Create Devices.sol

---
+++ AssetRegistry/Section/Devices.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+contract  SmartPhonDevices {
+  
+      address public immutable Registry_ADDRESS;
+      
+      uint public immutable Total_UINT;
+      
+      constructor(uint total_Unit+
+   }
+
+}
+
